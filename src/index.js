749247a40420b8d5480e9e2d96e0e54c4ef7ddc4
maskurnia shidi <maskurshidi12@gmail.com>
2022-04-09 22:28:24 +0700

    fix proccess

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'
-import * as serviceWorker from './serviceWorker';
-import {RoomProvider} from './context'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { RoomProvider } from "./context";
+
+import "react-datepicker/dist/react-datepicker.css";
+
 ReactDOM.render(
   <React.StrictMode>
-  <RoomProvider>
-     <App />
-  </RoomProvider>
+    <RoomProvider>
+      <App />
+    </RoomProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
